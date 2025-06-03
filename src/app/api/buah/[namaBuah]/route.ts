2bcfb244b07385dc9912072de064d4e3b4735d42
andreaskevn <sekianterimakasih37@gmail.com>
2025-06-03 10:14:33 +0700

    codemod

--- src/app/api/buah/[namaBuah]/route.ts
+++ src/app/api/buah/[namaBuah]/route.ts
@@ -3,8 +3,6 @@ import prisma from "@/lib/prisma";
 import type { FruitData } from "@/lib/definition";
 
 export async function GET(
-/* @next-codemod-error <suggested message> */
-/* @next-codemod-ignore */
   request: NextRequest,
   context: { params: { namaBuah: string } }
 ) {
