6f29781799f5e2c3bd83020d005f8635ef1bac54
andreaskevn <sekianterimakasih37@gmail.com>
2025-06-03 13:40:22 +0700

    nextconfig

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,19 @@ import type { NextConfig } from "next";
 import { NextConfig as NextConfigType } from "next";
 
 const nextConfig: NextConfigType = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**vercel-storage.com", // Ini akan mencocokkan semua subdomain dari vercel-storage.com
+      },
+    ],
+    // Atau jika Anda lebih suka menggunakan 'domains' secara eksplisit:
+    // domains: [
+    //   'your-specific-blob-id.public.blob.vercel-storage.com', // Ganti dengan domain Vercel Blob spesifik Anda
+    //   'public.blob.vercel-storage.com', // Jika ada
+    // ],
+  },
   webpack: (config) => {
     config.watchOptions = {
       ignored: [
