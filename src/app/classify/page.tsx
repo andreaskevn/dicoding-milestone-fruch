418beaa05487859cfd8c30eb2ff92c49e42ad024
andreaskevn <sekianterimakasih37@gmail.com>
2025-06-03 08:53:38 +0700

    changes at classify page

--- src/app/classify/page.tsx
+++ src/app/classify/page.tsx
@@ -98,7 +98,7 @@ export default function ClassifyPage() {
                   className="w-6 h-6 mr-2 text-green-500"
                   fill="none"
                   stroke="currentColor"
-                  viewBox="0 0 24 24"
+                  viewBox="0 0 24 24" 
                 >
                   <path
                     strokeLinecap="round"
