ea7123366e612479795fb76ec4e91040ffeafefa
andreaskevn <sekianterimakasih37@gmail.com>
2025-06-03 09:04:26 +0700

    merge all, input real model

--- src/generated/prisma/edge.js
+++ src/generated/prisma/edge.js
@@ -151,7 +151,7 @@ const config = {
       "value": "prisma-client-js"
     },
     "output": {
-      "value": "/root/dicoding/dicoding-milestone-fruch/src/generated/prisma",
+      "value": "D:\\Materi\\Kuliah\\Kerjaan\\Asisten Dosen\\PBO\\Code\\DICODING\\dicoding-milestone-fruch\\src\\generated\\prisma",
       "fromEnvVar": null
     },
     "config": {
@@ -160,12 +160,12 @@ const config = {
     "binaryTargets": [
       {
         "fromEnvVar": null,
-        "value": "debian-openssl-3.0.x",
+        "value": "windows",
         "native": true
       }
     ],
     "previewFeatures": [],
-    "sourceFilePath": "/root/dicoding/dicoding-milestone-fruch/prisma/schema.prisma",
+    "sourceFilePath": "D:\\Materi\\Kuliah\\Kerjaan\\Asisten Dosen\\PBO\\Code\\DICODING\\dicoding-milestone-fruch\\prisma\\schema.prisma",
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
@@ -179,7 +179,6 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
--- src/generated/prisma/index.js
+++ src/generated/prisma/index.js
@@ -152,7 +152,7 @@ const config = {
       "value": "prisma-client-js"
     },
     "output": {
-      "value": "/root/dicoding/dicoding-milestone-fruch/src/generated/prisma",
+      "value": "D:\\Materi\\Kuliah\\Kerjaan\\Asisten Dosen\\PBO\\Code\\DICODING\\dicoding-milestone-fruch\\src\\generated\\prisma",
       "fromEnvVar": null
     },
     "config": {
@@ -161,12 +161,12 @@ const config = {
     "binaryTargets": [
       {
         "fromEnvVar": null,
-        "value": "debian-openssl-3.0.x",
+        "value": "windows",
         "native": true
       }
     ],
     "previewFeatures": [],
-    "sourceFilePath": "/root/dicoding/dicoding-milestone-fruch/prisma/schema.prisma",
+    "sourceFilePath": "D:\\Materi\\Kuliah\\Kerjaan\\Asisten Dosen\\PBO\\Code\\DICODING\\dicoding-milestone-fruch\\prisma\\schema.prisma",
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
@@ -180,7 +180,6 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
@@ -229,8 +228,8 @@ exports.PrismaClient = PrismaClient
 Object.assign(exports, Prisma)
 
 // file annotations for bundling tools to include these files
-path.join(__dirname, "libquery_engine-debian-openssl-3.0.x.so.node");
-path.join(process.cwd(), "src/generated/prisma/libquery_engine-debian-openssl-3.0.x.so.node")
+path.join(__dirname, "query_engine-windows.dll.node");
+path.join(process.cwd(), "src/generated/prisma/query_engine-windows.dll.node")
 // file annotations for bundling tools to include these files
 path.join(__dirname, "schema.prisma");
 path.join(process.cwd(), "src/generated/prisma/schema.prisma")
